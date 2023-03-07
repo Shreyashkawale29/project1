279b1b854338766b0e1b1ac7148a2ff010549975
Shreyash <kawalesn01@gmail.com>
2023-03-07 15:21:12 +0530

    basics of js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello World")
+// let age = 20;
+// let Name = 'shreyash';
+// console.log(age)
+// console.log(typeof Name)
+
+let arr = [1,2,3,4,5]
+arr.push('name')
+arr.push(false)
+console.log(arr)
+
+let Name = "shreyash"
+console.log(Name.toUpperCase())
+
